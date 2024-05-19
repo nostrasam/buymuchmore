06610cbfd5081b8091a1cfcd4f4a8b9ed41f1dcd
nostrasam <nostrasam@yahoo.com>
2024-05-19 07:30:17 +0100

    IP change

--- buymuchmore/settings.py
+++ buymuchmore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-sj-99#p&&4um7qav$y#*f8mjxuuh#5&%j!@eo9g2gloyxn40!&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.94.145.172']
+ALLOWED_HOSTS = ['54.91.78.87']
 
 
 # Application definition
