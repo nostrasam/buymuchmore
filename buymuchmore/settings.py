ccd28bbb3f587d4b8f130dfb6c3bc1f727d4077c
nostrasam <nostrasam@yahoo.com>
2024-06-22 01:21:42 +0100

    Deploy eCommerce ip change

--- buymuchmore/settings.py
+++ buymuchmore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-sj-99#p&&4um7qav$y#*f8mjxuuh#5&%j!@eo9g2gloyxn40!&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.90.104.4']
+ALLOWED_HOSTS = ['54.197.70.135']
 
 
 # Application definition
