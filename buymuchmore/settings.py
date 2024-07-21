f669209b83af640e1f997d72ae177b0af903869f
nostrasam <nostrasam@yahoo.com>
2024-07-21 22:52:47 +0100

    Deployment Update

--- buymuchmore/settings.py
+++ buymuchmore/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sj-99#p&&4um7qav$y#*f8mjxuuh#5&%j!@eo9g2gloyxn40!&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['34.203.247.57']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -122,7 +122,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'home/static')]
 
 
 MEDIA_URL = 'media/'
